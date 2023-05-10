acdabcfb27274f117fd6ff0e8fdcbe5877351a3f
Khenissi <mkhenissi@ymail.com>
2023-05-10 23:16:55 +0200

    fetching

--- FRONT/my-vue-app/src/components/TasksPage.vue
+++ FRONT/my-vue-app/src/components/TasksPage.vue
@@ -62,7 +62,7 @@ export default {
     },
     methods: {
         handleDelete(taskId) {
-            axios.delete(`https://task-manager-gtp.up.railway.app/tasks/delete/${taskId}`, { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` } });
+            axios.delete(`https://task-manager-gtp.up.railway.app/tasks/delete/${taskId}`, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } });
             const index = this.tasks.findIndex(task => task.id === taskId);
             this.tasks.splice(index, 1);
         },
@@ -71,7 +71,7 @@ export default {
         }
     },
     async created() {
-        const response = await axios.get("https://task-manager-gtp.up.railway.app/tasks", { headers: { Authorization: `Bearer ${localStorage.getItem}` } });
+        const response = await axios.get("https://task-manager-gtp.up.railway.app/tasks", { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } });
         this.tasks = response.data;
     },
     components: { NavBar }
