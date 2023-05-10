7606fa1fa4c244ea10e281dcbf98880c83e0f14a
Khenissi <mkhenissi@ymail.com>
2023-05-10 21:10:42 +0200

    Raleway

--- BACK/app/db_connect.js
+++ BACK/app/db_connect.js
@@ -4,6 +4,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'Mohamed1',
+  port: 3306,
   database: 'task'
 });
 
--- BACK/index.js
+++ BACK/index.js
@@ -4,9 +4,6 @@ const express = require('express');
 const router = require('./app/routers');
 const cors = require('cors');
 
-const taskDatamapper = require('./app/datamappers/taskDatamapper')
-
-
 
 
 const port = process.env.PORT || 3000;
@@ -22,6 +19,3 @@ app.use(router);
 app.listen(port, () => {
   console.log(`Server ready: http://localhost:${port}`);
 });
-
-
-
