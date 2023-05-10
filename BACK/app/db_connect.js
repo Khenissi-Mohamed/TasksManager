499629cf1c73801f013ced7d263b36893a9cfffb
Khenissi <mkhenissi@ymail.com>
2023-05-10 21:28:58 +0200

    Raleway

--- BACK/app/db_connect.js
+++ BACK/app/db_connect.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Mohamed1',
-  port: 3306,
-  database: 'task'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
 });
 
 connection.connect((error) => {
--- BACK/index.js
+++ BACK/index.js
@@ -4,8 +4,6 @@ const express = require('express');
 const router = require('./app/routers');
 const cors = require('cors');
 
-
-
 const port = process.env.PORT || 3000;
 
 const app = express();
